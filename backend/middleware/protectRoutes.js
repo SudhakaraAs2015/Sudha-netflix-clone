c8da62e06a602bfc234453c379e1cfb7b4ce00b5
Sudhakara H N <sudhakarahn29@gmail.com>
2025-01-21 14:37:59 +0530

    Modified Index.CSS

--- backend/middleware/protectRoutes.js
+++ backend/middleware/protectRoutes.js
@@ -6,25 +6,56 @@ export const protectRoutes = async (req, res, next) => {
   try {
     const token = req.cookies["jwt-netflix"];
     if (!token) {
-      res
+      return res
         .status(401)
         .json({ success: false, message: "Unauthorized - No Token Provided" });
     }
+
     const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET_KEY);
     if (!decoded) {
-      res.status.json({
+      return res.status(401).json({
         success: false,
-        message: "Unathorized = Invalid Token",
+        message: "Unauthorized - Invalid Token",
       });
     }
+
     const user = await User.findById(decoded.userId).select("-password");
     if (!user) {
-      res.status(404).json({ success: false, message: "User not found" });
+      return res.status(404).json({ success: false, message: "User not found" });
     }
+
     req.user = user;
-    next();
+    next();  // Continue to the next middleware or route handler
   } catch (error) {
     console.log("Error in Middleware", error.message);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
+
+
+// export const protectRoutes = async (req, res, next) => {
+//   try {
+//     const token = req.cookies["jwt-netflix"];
+//     if (!token) {
+//       res
+//         .status(401)
+//         .json({ success: false, message: "Unauthorized - No Token Provided" });
+//     }
+//     const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET_KEY);
+//     if (!decoded) {
+//       res.status.json({
+//         success: false,
+//         message: "Unathorized = Invalid Token",
+//       });
+//     }
+//     const user = await User.findById(decoded.userId).select("-password");
+//     if (!user) {
+//       res.status(404).json({ success: false, message: "User not found" });
+//     }
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     console.log("Error in Middleware", error.message);
+//     res.status(500).json({ success: false, message: "Internal Server Error" });
+//   }
+// };
